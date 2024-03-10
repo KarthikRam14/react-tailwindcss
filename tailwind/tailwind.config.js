4d2f1b40c82ed9bb29319f6482d1cd28cdee7177
KarthikRam14 <karthikram1441@gmail.com>
2024-03-10 23:10:59 +0530

    updated

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/App.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
